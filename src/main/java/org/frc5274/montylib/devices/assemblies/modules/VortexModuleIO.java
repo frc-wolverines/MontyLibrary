ae2e230e849c4cb001851fe8418a1c0d7da9fc3f
frc-wolverines <rosemont5274@gmail.com>
2024-04-21 15:03:19 -0700

    changed SparkIO to VortexIO to reflect the purpose in VortexModuleIO

--- src/main/java/org/frc5274/montylib/devices/assemblies/modules/VortexModuleIO.java
+++ src/main/java/org/frc5274/montylib/devices/assemblies/modules/VortexModuleIO.java
@@ -1,13 +1,13 @@
 package org.frc5274.montylib.devices.assemblies.modules;
 
-import org.frc5274.montylib.devices.motors.SparkIO;
+import org.frc5274.montylib.devices.motors.VortexIO;
 import org.frc5274.montylib.devices.sensors.CANCoderIO;
 
 public class VortexModuleIO extends ModuleIO {
     public VortexModuleIO(ModuleConfig config) {
         super(
-            new SparkIO(config.drive_motor_config), 
-            new SparkIO(config.pivot_motor_config), 
+            new VortexIO(config.drive_motor_config), 
+            new VortexIO(config.pivot_motor_config), 
             new CANCoderIO(config.encoder_config)
         );
     }
